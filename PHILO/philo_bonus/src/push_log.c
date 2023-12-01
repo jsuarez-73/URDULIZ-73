ed12403bb123bcd983fd52adbb11774c7bea9076
jsuarez-73 <pingsuarezbuitago@gmail.com>
2023-12-01 16:04:01 +0100

    Optimization with emojis

--- PHILO/philo_bonus/src/push_log.c
+++ PHILO/philo_bonus/src/push_log.c
@@ -6,7 +6,7 @@
 /*   By: jsuarez- <jsuarez-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 21:17:33 by jsuarez-          #+#    #+#             */
-/*   Updated: 2023/12/01 15:46:55 by jsuarez-         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:00:10 by jsuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_push_log(t_gdata *gdt, char *log, t_state state)
 {
 	long	now;
+	char	*emoji;
 
 	sem_wait(gdt->s_death);
 	if (gdt->philo.state == DIED)
@@ -26,9 +27,10 @@ void	ft_push_log(t_gdata *gdt, char *log, t_state state)
 	if (gdt->philo.state != state && gdt->philo.state != FEED)
 	{
 		gdt->philo.state = state;
+		emoji = ft_set_emoji(state);
 		sem_post(gdt->s_death);
 		sem_wait(gdt->s_term);
-		printf("%ld %u %s\n", now, gdt->philo.id, log);
+		printf("%ld %u %s %s\n", now, gdt->philo.id, log, emoji);
 		if (state != DIED)
 			sem_post(gdt->s_term);
 	}
